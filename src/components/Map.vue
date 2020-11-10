7648947f291e5325d42d6f7c8e858bc61367cfb2
Dario Arrieta <arrieta.dario@hotmail.com>
2020-11-10 15:18:20 -0500

    cambios antes de cambiar todo

---
+++ src/components/Map.vue
@@ -0,0 +1,19 @@
+<template>
+<main>
+
+  <GoogleMap class="w-full h-full"
+    :center="{ lat: 38.423733, lng: 27.142826 }"
+    :zoom="4"
+    :clickActive="true"
+    :getLatLng="true"
+    :mapType="'hybrid'"
+    :moveActive="false"
+  />
+</main>
+</template>
+
+<script>
+    export default {
+        name: 'PruebaMap'
+    }
+</script>
---
+++ src/views/PruebaMap.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="w-full h-full">
+      <GoogleMap class="w-full h-64"
+        :center="{ lat: 38.423733, lng: 27.142826 }"
+        :zoom="4"
+        :clickActive="true"
+        :getLatLng="true"
+        :mapType="'hybrid'"
+        :moveActive="false"
+    />
+  </main>
+</template>
+
+<script>
+    export default {
+        name: 'PruebaMap'
+    }
+</script>
