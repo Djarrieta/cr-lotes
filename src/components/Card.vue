becc26ccb556e9bfa45fea4baebb79af86fd5912
Dario Arrieta <arrieta.dario@hotmail.com>
2020-11-12 11:37:37 -0500

    detalle card funcionando con un error en console

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,34 +2,34 @@
   <section class="w-full shadow-lg overflow-hidden rounded-lg border">
       <!-- Foto -->
       <img
-            v-if="propiedad.s8_pictures[0].fileUrl"
+            v-if="prop.s8_pictures[0].fileUrl"
             class="w-full h-48 object-scale-down object-center bg-gray-100"
-            :src="propiedad.s8_pictures[0].fileUrl"
+            :src="prop.s8_pictures[0].fileUrl"
         /> 
         <!-- Detalles -->
         <div class="pl-2 py-2 bg-gradient-to-r from-gray-900 via-gray-700 w-full">
             <p class="px-5 rounded-lg text-white font-poppins">
-                <i class="fas fa-map-marked-alt text-white"></i> {{ propiedad.s2_namePrvSelected }} - {{ propiedad.s2_nameCtnSelected }}
+                <i class="fas fa-map-marked-alt text-white"></i> {{ prop.s2_namePrvSelected }} - {{ prop.s2_nameCtnSelected }}
             </p>
         </div>
         <div class="px-6 py-5 bg-gray-200">
             <p class="font-bold text-md mb-2 uppercase">
-                {{ propiedad.s1_title }}
+                {{ prop.s1_title }}
             </p>
             <p class="font-medium text-md text-orange-600 mb-2">
-                {{ propiedad.s1_price  | precio }}
+                {{ prop.s1_price  | precio }}
             </p>
             <p class="text-gray-700 text-xs">
-                <!-- {{ propiedad.descripcion.substring(0,100)+"..." }} -->
-                {{ propiedad.s1_description.substring(0,100)+"..." }}
+                <!-- {{ prop.descripcion.substring(0,100)+"..." }} -->
+                {{ prop.s1_description.substring(0,100)+"..." }}
             </p>
             <div class="flex justify-between mt-5">
                 <p>
-                    <i class="fas fa-ruler-combined"></i> {{propiedad.s1_area}} {{propiedad.s1_areaUn}}
+                    <i class="fas fa-ruler-combined"></i> {{prop.s1_area}} {{prop.s1_areaUn}}
                 </p>
                 <div>
-                    <router-link :to="'/perfil-propiedad/'+propiedad.propid" class="bg-gray-600 px-4 py-2 rounded-sm mr-5 text-white hover:text-white h-6">Detalles</router-link>
-                    <a :href="'https://api.whatsapp.com/send?text=CR-Lotes%20https://localhost:8080/perfil-propiedad/'+propiedad.propid" class="text-gray-900 hover:text-orange-700" target="_blank"><i class="fab fa-whatsapp"></i></a>
+                    <router-link :to="'/perfil-propiedad/'+prop.propId" class="bg-gray-600 px-4 py-2 rounded-sm mr-5 text-white hover:text-white h-6">Detalles</router-link>
+                    <a :href="'https://api.whatsapp.com/send?text=CR-Lotes%20https://localhost:8080/perfil-propiedad/'+prop.propId" class="text-gray-900 hover:text-orange-700" target="_blank"><i class="fab fa-whatsapp"></i></a>
                 </div>
             </div>
         </div>
@@ -40,9 +40,7 @@
 import firebase from "firebase"
 export default {
     name:"Card",
-    props:{
-        propiedad:{ type: Object, required: true },
-    },
+    props:["prop"],
     data(){
         return {
             infoVendedor: '',
@@ -61,8 +59,8 @@ export default {
         
     },
     filters: {
-        precio: function(value){
-            let precioFormateado = new Intl.NumberFormat().format(value)
+        precio: function(val){
+            let precioFormateado = new Intl.NumberFormat().format(val)
             return precioFormateado + "₡"
         }
     }
